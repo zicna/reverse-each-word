06533dad4e3df0e1cbc41a69c8c035ea3ce6b9ce
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-17 15:53:23 -0600

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,23 +1,20 @@
-def reverse_each_word(string)
-    arr_of_strings = string.split(" ")
-    #puts arr_of_strings
-    arr_of_reversed_strings = []
-    arr_of_strings.each do |word|
-        arr_of_reversed_strings << word.reverse
-    end
-    arr_of_strings.collect do |word|
-        word.reverse
-    end
- arr_of_reversed_strings.join(" ")
-end
-
-#def reverse_each_word_using_collect(string)
- #   arr_of_strings = string.split(" ")
-  #  arr_of_strings.map do |word|
-   #     word.reverse
-    #end
+#def reverse_each_word(string)
+#     #this will convert string into array of strings 
+#     #since new words are separated with " " that is what we pass as argument for .split method
+#     arr_of_strings = string.split(" ")
+#     #since implicit return of .each method is original array on what it was called we need new empty array to collect changed items
+#     arr_of_reversed_strings = []
+#     arr_of_strings.each do |word|
+#         arr_of_reversed_strings << word.reverse
+#     end
+#     arr_of_strings.collect do |word|
+#         word.reverse
+#     end
+#  arr_of_reversed_strings.join(" ")
 #end
 
+def reverse_each_word(string)
+    string.split(' ').collect {|word| word.strip().reverse}.join(' ')
+end
 
-#reverse_each_word("Milanu se jako sere. molim te pozuri!")
-#reverse_each_word_using_collect("Milanu se jako sere. molim te pozuri!")
+#puts reverse_each_word("olleH ,ereht dna woh era ?uoy")
